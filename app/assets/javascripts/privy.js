b957cf4e7e5bc69498ba2d32a461722852642c02
vaskaloidis <vas.kaloidis@gmail.com>
2020-07-26 17:42:44 -0400

    Init

---
+++ app/assets/javascripts/privy.js
@@ -0,0 +1,9 @@
+var _d_site = _d_site || 'A620F353BF55BB91D31BCF8D';
+(function () {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = document.location.protocol + '//widget.privy.com/assets/widget.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(script, s);
+})();
