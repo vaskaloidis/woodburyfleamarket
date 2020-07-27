1959e3e8176c0f5ce132969111dcaa42d2708667
vaskaloidis <vas.kaloidis@gmail.com>
2020-07-27 08:40:49 -0400

    Added Heroku prod logging. Added users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(email: 'vas.kaloidis@gmail.com', password: 'Spartan11!', password_confirmation: 'Spartan11!')
-User.create!(email: 'thenewwoodburyfleamarket@gmail.com', password: 'Piper787br', password_confirmation: 'Piper787br')
+# User.create!(email: 'vas.kaloidis@gmail.com', password: 'Spartan11!', password_confirmation: 'Spartan11!')
+# User.create!(email: 'thenewwoodburyfleamarket@gmail.com', password: 'Piper787br', password_confirmation: 'Piper787br')
 
-# Content.create!(section: "home", content: "The market is open today Saturday September 28th and Sunday the 29th!!<br> We look forward to seeing you here!")
+Content.create!(section: "home", content: "The market is open today Saturday September 28th and Sunday the 29th!!<br> We look forward to seeing you here!")
