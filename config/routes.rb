bf43cb739b5e06d62a4dcf9274b941842b93c6a9
vaskaloidis <vas.kaloidis@gmail.com>
2020-07-28 18:27:28 -0400

    Removed devise registration. Updated home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-	get 'pages/home', to: 'high_voltage/pages#show', id: 'home'
+  devise_for :users, :skip => [:registrations]
+  as :user do
+	  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+	  put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
+  get 'pages/home', to: 'high_voltage/pages#show', id: 'home'
 	get 'pages/about', to: 'high_voltage/pages#show', id: 'about'
 	get 'pages/directions', to: 'high_voltage/pages#show', id: 'directions'
 	get 'pages/gus', to: 'high_voltage/pages#show', id: 'gus'
