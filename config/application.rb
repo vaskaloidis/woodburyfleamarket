fa01b199513c7480f3875adb4b587c10a3bccbf8
Vas Kaloidis <vas.kaloidis@gmail.com>
2023-02-20 22:46:40 -0500

    Updated precompile config

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Woodburyflea
     config.load_defaults 5.2
 
     config.action_controller.permit_all_parameters = true
+    config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
